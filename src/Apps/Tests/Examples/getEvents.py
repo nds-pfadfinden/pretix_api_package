d635c6c06b72d795921c1bd1acb812279c0b3c1b
Yannik Menge <yannik.menge@tuhh.de>
2023-12-05 12:46:46 +0100

    bsp für get events

---
+++ src/Apps/Tests/Examples/getEvents.py
@@ -0,0 +1,13 @@
+import pathlib
+
+from Tools.Pretix_API import Pretix_API
+
+path = str(pathlib.Path(__file__).parent.resolve())
+
+
+
+user = Pretix_API()
+data = user.get_events()
+
+user.print_to_file(path +"\\output.json",data)
+
