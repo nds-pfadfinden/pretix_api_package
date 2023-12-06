f1d79bac1c2a93b2866691675ab5c9a080136671
Yannik Menge <yannik.menge@tuhh.de>
2023-12-06 21:44:26 +0100

    changed file name

--- src/Apps/Tests/Examples/get_events.py
+++ src/Apps/Tests/Examples/get_events.py
@@ -9,5 +9,5 @@ path = str(pathlib.Path(__file__).parent.resolve())
 user = Pretix_API()
 data = user.get_events()
 
-user.print_to_file(path +"\\output.json",data)
+user.print_to_file(path +"\\get_events.json",data)
 
